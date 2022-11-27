c5ae57707e4f2faaf013663e0bc28315f1521d73
A.K.M Mainul Hasan <88575577+mhasanlab@users.noreply.github.com>
2022-11-27 18:55:08 +0600

    Update server.js

--- ClientApp/server.js
+++ ClientApp/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/client-app'));
+app.use(express.static('./dist/ClientApp'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/client-app/'}),
+    res.sendFile('index.html', {root: 'dist/ClientApp/'}),
 );
 
 // Start the app by listening on the default Heroku port
